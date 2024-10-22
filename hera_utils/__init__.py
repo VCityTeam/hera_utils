de4185026a4bc9af065896196a0b03920d90eace
EricBoix <eric.boix@insa-lyon.fr>
2024-10-22 17:22:25 +0200

    Removing unused environment.

--- hera_utils/__init__.py
+++ hera_utils/__init__.py
@@ -2,7 +2,6 @@ from .version import version
 from .utils import print_version, check_version, assert_version, clear_workflow_template
 from .parser import parser
 from .argo_server import argo_server
-from .num_exp_environment import num_exp_environment, Struct
 
 __version__ = version
 __title__ = "hera_utils"
